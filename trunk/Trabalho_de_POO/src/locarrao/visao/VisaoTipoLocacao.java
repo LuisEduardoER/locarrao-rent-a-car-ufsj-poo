0a6cbdaf3616384e2a2dec4ee63bb51a5c848750
joao.dk16@gmail.com <joao.dk16@gmail.com@52dbeed6-62d6-36f8-2340-52c633b637ce>
2011-09-28 18:05:32 +0000

    começo de trabalho

--- trunk/Trabalho_de_POO/src/locarrao/visao/VisaoTipoLocacao.java
+++ trunk/Trabalho_de_POO/src/locarrao/visao/VisaoTipoLocacao.java
@@ -1,41 +1,54 @@
 package locarrao.visao;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import modelo.dominio.TipoVeiculo;
+import modelo.dominio.TipoLocacao;
+import modelo.persistencia.PersistenciaTipoVeiculo;
 
 public class VisaoTipoLocacao {
-    public void cadastraTipoLocacao() /*throws FileNotFoundException, IOException*/ {
-        /*
-        //digitar os dados
-        Scanner digitar = new Scanner(System.in);
-        //menu para a seleção do tipo
-        List listaTipoVeiculos = new ArrayList<TipoVeiculo>();
+    public void cadastraTipoLocacao() {
+        TipoLocacao tipoLocacao = new TipoLocacao();
+        /* digitar os dados */
+        Scanner entrada = new Scanner(System.in);
         
-        File arquivo = new File("src/arquivos/TipoVeiculo.txt");
-        if(arquivo.exists()){
-            FileReader reader = new FileReader(arquivo);
-            BufferedReader leitor = new BufferedReader(reader);
-            TipoVeiculo tipoVeiculo = new TipoVeiculo();
-            String linha = null;
-            
-            while((linha = leitor.readLine())){
-                tipoVeiculo.setTipo(linha);
-                listaTipoVeiculos.add(tipoVeiculo);
-            }
-        }
-        else{
-            System.out.println("Erro: Arquivo nao encontrado");
-        }
-         * 
-         */
+        //Tipo
+        pesquisaTipoVeiculo(tipoLocacao);
+        
+        //outros dados
+        System.out.println("Tipo de Locaçao");
+        tipoLocacao.setNomeTipo(entrada.nextLine());
+        
+        System.out.println("Taxa base");
+        tipoLocacao.setTaxa(entrada.nextDouble());
+        
+        System.out.println("Preço por Quilometro");
+        tipoLocacao.setPrecoPorQuilometro(entrada.nextDouble());
+        
+        /* ------- Fim ------- */
+        
+        /* Salvar no arquivo */
+        
+        
         
     }
+    
+    public void pesquisaTipoVeiculo(TipoLocacao tipoLocacao){
+        VisaoTipoVeiculo visaoTipoVeiculo = new VisaoTipoVeiculo();
+        PersistenciaTipoVeiculo buscaTipoVeiculo = new PersistenciaTipoVeiculo();
+        try {
+            int itemMenu = visaoTipoVeiculo.MenuTipoVeiculo(buscaTipoVeiculo.
+                    retornarTodosTipoVeiculo());
+            
+            tipoLocacao.setTipoVeiculo(buscaTipoVeiculo.retornaTipoVeiculo(itemMenu));
+        } catch (FileNotFoundException ex) {
+            System.out.println("Erro: não achou o arquivo!");
+        } catch (IOException ex) {
+            System.out.println("Erro: na escrita/leitura do arquivo!");
+        }
+    }
+    
+
+    
          
 }
