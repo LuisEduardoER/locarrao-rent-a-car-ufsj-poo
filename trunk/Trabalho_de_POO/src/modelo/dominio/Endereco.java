5d81303a39f78ff560c5d105ebb4bd743bb9e76a
thaisccoufsj <thaisccoufsj@52dbeed6-62d6-36f8-2340-52c633b637ce>
2011-09-30 16:49:01 +0000

    implementação de construtores de clientes,endereco e funcionarios.

--- trunk/Trabalho_de_POO/src/modelo/dominio/Endereco.java
+++ trunk/Trabalho_de_POO/src/modelo/dominio/Endereco.java
@@ -3,11 +3,11 @@ package modelo.dominio;
 public class Endereco {
  
 	private String rua;
-	 
+        
 	private int numero;
-	 
+        
 	private char complemento;
-	 
+        
 	private String bairro;
 	 
 	private String cidade;
@@ -15,6 +15,16 @@ public class Endereco {
 	private String uf;
 	 
 	private String cep;
+        
+        public Endereco(String rua,int numero,char complemento,String bairro,String cidade,String uf,String cep){
+            this.rua=rua;
+            this.numero=numero;
+            this.complemento=complemento;
+            this.bairro=bairro;
+            this.cidade=cidade;
+            this.uf=uf;
+            this.cep=cep;
+        }
 	 
 }
  
--- trunk/Trabalho_de_POO/src/modelo/dominio/Funcionarios.java
+++ trunk/Trabalho_de_POO/src/modelo/dominio/Funcionarios.java
@@ -7,6 +7,12 @@ public class Funcionarios extends Pessoa {
 	private String usuario;
 	 
 	protected String senha;
+        
+        public Funcionarios (String cargo,String usuario,String senha){
+            this.cargo=cargo;
+            this.usuario=usuario;
+            this.senha=senha;
+        }
 	 
 }
  
