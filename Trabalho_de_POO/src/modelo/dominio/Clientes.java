1e3bc34755c312574f7fef3c0975c11d19124f1b
thaisccoufsj <thaisccoufsj@52dbeed6-62d6-36f8-2340-52c633b637ce>
2011-09-30 16:49:01 +0000

    implementação de construtores de clientes,endereco e funcionarios.

--- Trabalho_de_POO/src/modelo/dominio/Clientes.java
+++ Trabalho_de_POO/src/modelo/dominio/Clientes.java
@@ -1,14 +1,18 @@
 package modelo.dominio;
 
-import modelo.dominio.Endereco;
+/*import modelo.dominio.Endereco;
 import modelo.dominio.Motorista;
-import modelo.dominio.Pessoa;
+import modelo.dominio.Pessoa;*/
 
 public class Clientes extends Pessoa {
  
 	private Endereco endereco;
-	 
 	private Motorista motorista;
+        public Clientes(Endereco endereco,Motorista motorista){
+            this.endereco=endereco;
+            this.motorista=motorista;
+        }
+        
 	 
 }
  
