789e56f0796febd2451e38094a5b66aa7d8d2734
paty-complicated@hotmail.com <paty-complicated@hotmail.com@52dbeed6-62d6-36f8-2340-52c633b637ce>
2011-10-03 20:01:02 +0000

    correção dos dominios e criação dos getters e setters

--- trunk/Trabalho_de_POO/src/modelo/dominio/ModeloVeiculo.java
+++ trunk/Trabalho_de_POO/src/modelo/dominio/ModeloVeiculo.java
@@ -3,6 +3,20 @@ package modelo.dominio;
 public class ModeloVeiculo {
  
 	private String modelo;
+
+    /**
+     * @return the modelo
+     */
+    protected String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    protected void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
 	 
 }
  
--- trunk/Trabalho_de_POO/src/modelo/dominio/Motorista.java
+++ trunk/Trabalho_de_POO/src/modelo/dominio/Motorista.java
@@ -4,6 +4,34 @@ public class Motorista extends Pessoa {
  
 	private long cnh;
         private Endereco endereco;
+
+    /**
+     * @return the cnh
+     */
+    protected long getCnh() {
+        return cnh;
+    }
+
+    /**
+     * @param cnh the cnh to set
+     */
+    protected void setCnh(long cnh) {
+        this.cnh = cnh;
+    }
+
+    /**
+     * @return the endereco
+     */
+    protected Endereco getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    protected void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 	 
 }
  
