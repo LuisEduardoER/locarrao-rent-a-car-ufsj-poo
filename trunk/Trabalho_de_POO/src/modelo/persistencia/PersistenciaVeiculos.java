6aa7bb349972a4819a4c536012781534ed2bbb7a
paty-complicated@hotmail.com <paty-complicated@hotmail.com@52dbeed6-62d6-36f8-2340-52c633b637ce>
2011-10-04 21:22:32 +0000

    criação de arquivo funcionarios persistenciaFuncionarios VisaoFuncionarios correção Motorista.java

---
+++ trunk/Trabalho_de_POO/src/modelo/persistencia/PersistenciaVeiculos.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.persistencia;
+
+/**
+ *
+ * @author PATY
+ */
+public class PersistenciaVeiculos {
+    
+}
