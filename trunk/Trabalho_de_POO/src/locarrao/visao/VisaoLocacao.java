056d31364955f3555fd8ac19e80c2c2546ed0a08
joao.dk16@gmail.com <joao.dk16@gmail.com@52dbeed6-62d6-36f8-2340-52c633b637ce>
2011-10-05 07:03:44 +0000

    Reformulação do Menu pronta. Inicio dos testes

---
+++ trunk/Trabalho_de_POO/src/locarrao/visao/VisaoLocacao.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package locarrao.visao;
+
+/**
+ *
+ * @author Magelinha
+ */
+public class VisaoLocacao {
+    public void cadastraLocacao(){
+        
+    }
+    
+    public void alteraLocacao(){
+        
+    }
+    
+    public void pesquisaLocacao(){
+        
+    }
+    
+    public void excluiLocacao(){
+        
+    }
+    
+    public void locacoesEmAberto(){
+        
+    }
+    
+    public void locacoesFinalizadas(){
+        
+    }
+    
+    public void veiculosDisponiveis(){
+        
+    }
+    
+    public void veiculosMaisProcurados(){
+        
+    }
+    
+    public void veiculosMaisRentaveis(){
+        
+    }
+    
+    public void disponibilidadePorTipo(){
+        
+    }
+}
