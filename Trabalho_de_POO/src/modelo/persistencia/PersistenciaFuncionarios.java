b8c417cb1fdf6078477d7350131d11f73c9d99dc
thaisccoufsj <thaisccoufsj@52dbeed6-62d6-36f8-2340-52c633b637ce>
2011-10-19 00:59:35 +0000

    Reformulando funcionarios e otimizando validacao.

--- Trabalho_de_POO/src/modelo/persistencia/PersistenciaFuncionarios.java
+++ Trabalho_de_POO/src/modelo/persistencia/PersistenciaFuncionarios.java
@@ -15,125 +15,123 @@ import java.util.ArrayList;
 import java.util.List;
 import modelo.dominio.Endereco;
 import modelo.dominio.Funcionarios;
+import locarrao.visao.VisaoFuncionarios;
 
 /**
  *
  * @author PATY
  */
 public class PersistenciaFuncionarios {
-    File arquivo = new File("src/arquivos/Funcionarios.txt");
-    public List<Funcionarios> retornaTodosFuncionarios () throws FileNotFoundException, IOException {
-        
-        //criando lista de funcionarios
-        List<Funcionarios> listaFuncionarios = new ArrayList<Funcionarios> ();
-        
+    public static File arquivo;
+    public static List<Funcionarios> listaFuncionarios;
+    
+    VisaoFuncionarios visaoFuncionarios = new VisaoFuncionarios();
+   
+    public PersistenciaFuncionarios(){
+        arquivo = new File("src/arquivos/Funcionarios.txt");
+        listaFuncionarios = new ArrayList<Funcionarios> ();
+        retornarTodosFuncionarios();
+    }
+    public static void retornarTodosFuncionarios(){
         if(arquivo.exists()){
+            FileReader reader = null;
+            try{
+                reader = new FileReader(arquivo);
+                BufferedReader leitor = new BufferedReader(reader);
+                
+                //percorre o arquivo
+                String linha = null;
+                int contador = 0;
+                Funcionarios funcionarios = new Funcionarios();
+                Endereco endereco = new Endereco();
+                
+                try{
+                    while((linha = leitor.readLine()) !=null){
+                       if (contador==0){
+                            funcionarios.setCodigo(Integer.parseInt(linha));
+                            contador++;
+                        }
+                       else if(contador==1){
+                            funcionarios.setNome(linha);
+                            contador++;
+                        }
+                        else if (contador==2){
+                            funcionarios.setCpf(linha);
+                            contador++;
+                        }
+                        else if (contador==3){
+                            funcionarios.setTefefone(linha);
+                            contador++;
+                        }
+                        else if (contador==4){
+                            funcionarios.setCargo(linha);
+                            contador++;
+                        }
+                        else if (contador==5){
+                            funcionarios.setUsuario(linha);
+                            contador++;
+                        }
+                        else if (contador==6){
+                            funcionarios.setSenha(linha);
+                            contador++;
+                        }
+                        else if (contador==7){
+                         endereco.setRua(linha);
+                         contador++;
+                        }
+                        else if (contador==8) {
+                          endereco.setNumero(Integer.valueOf(linha));
+                          contador++;
+                        }
+                        else if (contador==9){
+                            endereco.setComplemento(linha);
+                            contador++;
+                        }
+                        else if (contador==10){
+                            endereco.setBairro(linha);
+                            contador++;
+                        }
+                        else if (contador==11){
+                            endereco.setCidade(linha);
+                            contador++;
+                        }
+                        else if (contador==12){
+                            endereco.setUf(linha);
+                            contador++;
+                        }
+                        else if (contador==13){
+                            endereco.setCep(linha);
+                            funcionarios.setEndereco(endereco);
+                            listaFuncionarios.add(funcionarios);
+                            contador=0;
+                            funcionarios = new Funcionarios();
+                            endereco = new Endereco();
+                        }
+                    }
+                    reader.close();
+                    leitor.close();
+                }catch (IOException ex) {
+                    System.out.println("Erro na leitura/escrita do arquivo");
+                    System.out.println(ex.getMessage());
+                }
+            }catch (FileNotFoundException ex) {
+                System.out.println("Arquivo não encontrado");
+            }
             
-            //lendo o arquivo...
-            FileReader reader = new FileReader(arquivo);
-            BufferedReader leitor = new BufferedReader(reader);
-            
-            Funcionarios funcionarios = new Funcionarios();
-            Endereco endereco = new Endereco();
-            
-            //percorrendo o arquivo
-            String linha = null;
-            int contador=0;
-            while(leitor.ready()){
-                if (contador==0){
-                    linha=leitor.readLine();
-                    
-                    //transformando string em inteiro...
-                    funcionarios.setCodigo(Integer.valueOf(linha));
-                    contador++;
-                } 
-                else if(contador==1){
-                    linha=leitor.readLine();
-                    funcionarios.setNome(linha);
-                    contador++;
-                }
-                else if (contador==2){
-                    linha=leitor.readLine();
-                    funcionarios.setCpf(linha);
-                    contador++;
-                }
-                else if (contador==3){
-                    linha=leitor.readLine();
-                    funcionarios.setTefefone(linha);
-                    contador++;
-                }
-                else if (contador==4){
-                    linha=leitor.readLine();
-                    funcionarios.setCargo(linha);
-                    contador++;
-                }
-                else if (contador==5){
-                    linha=leitor.readLine();
-                    funcionarios.setUsuario(linha);
-                    contador++;
-                }
-                else if (contador==6){
-                    linha=leitor.readLine();
-                    funcionarios.setSenha(linha);
-                    contador++;
-                }
-                else if (contador==7){
-                    linha=leitor.readLine();
-                    endereco.setRua(linha);
-                    contador++;
-                }
-                else if (contador==8) {
-                    linha=leitor.readLine();
-                    endereco.setNumero(Integer.valueOf(linha));
-                    contador++;
-                }
-                else if (contador==9){
-                    linha=leitor.readLine();
-                    endereco.setComplemento(linha);
-                    contador++;
-                }
-                else if (contador==10){
-                    linha=leitor.readLine();
-                    endereco.setBairro(linha);
-                    contador++;
-                }
-                else if (contador==11){
-                    linha=leitor.readLine();
-                    endereco.setCidade(linha);
-                    contador++;
-                }
-                else if (contador==12){
-                    linha=leitor.readLine();
-                    endereco.setUf(linha);
-                    contador++;
-                }
-                else if (contador==13){
-                    linha=leitor.readLine();
-                    endereco.setCep(linha);
-                    funcionarios.setEndereco(endereco);
-                    listaFuncionarios.add(funcionarios);
-                    contador=0;
-                }
-                reader.close();
-                leitor.close();
-            }    
         }
-        else {
+        else{
             System.out.println("Arquivo não encontrado");
         }
-        return listaFuncionarios;    
-   }
-    public boolean salvar(List<Funcionarios> listaFuncionarios,Funcionarios funcionarios) throws FileNotFoundException, IOException{
-        
-        List<Funcionarios> listaClientes = new ArrayList<Funcionarios> ();
-        listaClientes=retornaTodosFuncionarios();
-        
+    }
+  
+    public boolean salvar(Funcionarios funcionarios) throws FileNotFoundException, IOException{
+  
         FileWriter writer = new FileWriter(arquivo);
         PrintWriter cadastro = new PrintWriter(writer);
         
-        boolean encontrou = pesquisaFuncionarios(listaFuncionarios, funcionarios);
-        if(!encontrou){
-            for(Funcionarios cadastroFuncionarios: listaClientes){
+        listaFuncionarios.add(funcionarios);
+        
+            for(Funcionarios cadastroFuncionarios: listaFuncionarios){
                 cadastro.println(cadastroFuncionarios.getCodigo());
                 cadastro.println(cadastroFuncionarios.getNome());
                 cadastro.println(cadastroFuncionarios.getCpf());
@@ -149,19 +147,16 @@ public class PersistenciaFuncionarios {
                 cadastro.println(cadastroFuncionarios.getEndereco().getUf());
                 cadastro.println(cadastroFuncionarios.getEndereco().getCep());
             } 
-            return false;
+            writer.close();
+            cadastro.close();
+            
+            return true;
+            
         }
-        else {
-            return false;
-        }
+ 
+    public boolean alteraFuncionarios(Funcionarios funcionarios) throws FileNotFoundException, IOException{
         
-    }
-    
-    public boolean alteraFuncionarios(List<Funcionarios> listaFuncionarios,Funcionarios funcionarios) throws FileNotFoundException, IOException{
-        
-        listaFuncionarios=retornaTodosFuncionarios();
-        
-        boolean existe = pesquisaFuncionarios(listaFuncionarios, funcionarios);
+        boolean existe = pesquisaFuncionarios(funcionarios);
         boolean retorno = false;
         if (existe){
             for(Funcionarios modificaFuncionarios: listaFuncionarios){
@@ -177,17 +172,72 @@ public class PersistenciaFuncionarios {
         }
         return retorno;
     }
-    public boolean pesquisaFuncionarios(List<Funcionarios> listaFuncionarios,Funcionarios funcionarios) throws FileNotFoundException, IOException{
-        
-        listaFuncionarios=retornaTodosFuncionarios();
+    public boolean pesquisaFuncionarios(Funcionarios funcionarios) throws FileNotFoundException, IOException{
         
         boolean encontrou = false;
         for(Funcionarios achaFuncionarios: listaFuncionarios){
-            if (achaFuncionarios.getCodigo() == achaFuncionarios.getCodigo()){
+            if (achaFuncionarios.getCpf().equals(funcionarios.getCpf())){
                 return encontrou=true;
             }
         }
         return encontrou;
     }
-    
+    public boolean pesquisaFuncionarios(int codigo){
+        boolean encontrou = false;
+        for(Funcionarios funcionario : listaFuncionarios){
+            if(funcionario.getCodigo()==codigo){
+                encontrou = true;
+            }
+            
+        }
+        return encontrou;
+    }
+    public boolean retornarFuncionario(Funcionarios funcionarios){
+        boolean achou = true;
+        for(Funcionarios item : listaFuncionarios){
+            if(item.getCodigo()== funcionarios.getCodigo()){
+                achou=true;
+                funcionarios.setCargo(item.getCargo());
+                funcionarios.setNome(item.getNome());
+                funcionarios.setCpf(item.getCpf());
+                funcionarios.setTefefone(item.getTefefone());
+                funcionarios.setEndereco(item.getEndereco());
+            }
+            
+        }
+        return achou;
+    }
+    public boolean retornarClienteComCpf(Funcionarios funcionarios){
+        boolean achou = false;
+        for(Funcionarios item : listaFuncionarios){
+            if(item.getCpf().equals(funcionarios.getCpf())){
+                achou = true;
+                funcionarios.setNome(item.getNome());
+                funcionarios.setTefefone(item.getTefefone());
+                funcionarios.setEndereco(item.getEndereco());
+            }
+        }
+        
+        return achou;
+    }
+    public void mostrarFuncionariosComNome(String nome){
+        List<Funcionarios> lista = new ArrayList<Funcionarios>();
+         visaoFuncionarios = new VisaoFuncionarios();
+         
+         for(Funcionarios item: listaFuncionarios){
+             if(item.getNome().equals(nome)){
+                 lista.add(item);
+             }
+         }
+         if(lista.isEmpty()){
+            System.out.println("Nenhum " + nome + "cadastrado");
+        }else{
+            System.out.println("------------- Funcionário -------------");
+            System.out.println();
+            for(Funcionarios item: lista){
+                visaoFuncionarios.imprimeFuncionarios(item);
+                System.out.println();
+            }
+        }   
+    }
 }
