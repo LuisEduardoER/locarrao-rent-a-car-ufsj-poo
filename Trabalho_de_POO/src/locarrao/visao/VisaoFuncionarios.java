0ee98416afbb3b83af5b1b3bd1364a38b4631e07
thaisccoufsj <thaisccoufsj@52dbeed6-62d6-36f8-2340-52c633b637ce>
2011-10-19 00:59:00 +0000

    Reformulando funcionarios e otimizando validacao.

--- Trabalho_de_POO/src/locarrao/visao/VisaoFuncionarios.java
+++ Trabalho_de_POO/src/locarrao/visao/VisaoFuncionarios.java
@@ -4,10 +4,8 @@
  */
 package locarrao.visao;
 
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import modelo.dominio.Endereco;
 import modelo.dominio.Funcionarios;
@@ -18,80 +16,124 @@ import modelo.dominio.Validacao;
  * @author PATY
  */
 public class VisaoFuncionarios {
-    public void cadastraFuncionarios() throws FileNotFoundException, IOException{
-        Funcionarios funcionarios = new Funcionarios();
+    PersistenciaFuncionarios persistenciaFuncionarios;
+    public static Funcionarios funcionarios;
+    VisaoEndereco visaoEndereco;
+    public VisaoFuncionarios(){
+        funcionarios = new Funcionarios();
+    }
+    public void cadastraFuncionarios() {
+        persistenciaFuncionarios = new PersistenciaFuncionarios();
+        funcionarios=new Funcionarios();
         Endereco endereco = new Endereco();
-        List<Funcionarios> listaFuncionarios = new ArrayList<Funcionarios>();
-        Validacao valida = new Validacao();
-        Scanner cadastro = new Scanner(System.in);
-        PersistenciaFuncionarios persistenciaFuncionarios = new PersistenciaFuncionarios();
-        listaFuncionarios = persistenciaFuncionarios.retornaTodosFuncionarios();
+        visaoEndereco = new VisaoEndereco();
         
-        System.out.println("Digite o codigo");
-        funcionarios.setCodigo(cadastro.nextInt());
-        boolean existe = false;
-        try {
-            existe = persistenciaFuncionarios.pesquisaFuncionarios(listaFuncionarios, funcionarios);
-            
-            if (existe){
-                    System.out.println("Digite o nome");
-                    funcionarios.setNome(cadastro.nextLine());
-                    System.out.println("Digite o cpf");
-                    String retorno = cadastro.nextLine();
-                    if(valida.validarCPF(retorno)){
-                         System.out.println("CPF VALIDO");
-                        funcionarios.setCpf(retorno);
-                        System.out.println("Digite o telefone");
-                        funcionarios.setTefefone(cadastro.nextLine());
-                        System.out.println("Digite o cargo");
-                        funcionarios.setTefefone(cadastro.nextLine());
-                        System.out.println("Digite o usuario");
-                        funcionarios.setTefefone(cadastro.nextLine());
-                        System.out.println("Digite a senha");
-                        funcionarios.setTefefone(cadastro.nextLine());
-                        System.out.println("Digite o rua");
-                        endereco.setRua(cadastro.nextLine());
-                        System.out.println("Digite o numero");
-                        endereco.setNumero(cadastro.nextInt());
-                        cadastro.nextLine();
-                        System.out.println("Digite o complemento");
-                        endereco.setComplemento(cadastro.nextLine());
-                        System.out.println("Digite o bairro");
-                        endereco.setBairro(cadastro.nextLine());
-                        System.out.println("Digite a Cidade");
-                        endereco.setCidade(cadastro.nextLine());
-                        System.out.println("Digite uf");
-                        endereco.setUf(cadastro.nextLine());
-                        System.out.println("Digite cep");
-                        retorno = cadastro.nextLine();
-                        if(valida.validarCEP(retorno)){
-                            System.out.println("CEP valido");
-                            endereco.setCep(retorno);
-                            funcionarios.setEndereco(endereco);
-                    
-                            boolean operacao = persistenciaFuncionarios.salvar(listaFuncionarios, funcionarios);
-                            if (operacao) {
-                                 System.out.println("Salvo com sucesso");
-                            }
-                            else {
-                                System.out.println("Erro!");
-                            }
+        //aparece na tela
+        Scanner cadastro =new Scanner(System.in);
+        String dado =null;
+        do{
+            System.out.println("Digite o cpf");
+            dado = cadastro.nextLine();
+            if(dado.isEmpty()){
+                System.out.println("É obrigatoria a digitação do CPF");
+            }
+        }while(dado.isEmpty());
+        
+        Validacao valida = new Validacao();
+        if(!valida.validarCPF(dado)){
+            System.out.println("CPF invalido");
+            return;
+        }else{
+            funcionarios.setCpf(dado);
+            try{
+                if(persistenciaFuncionarios.pesquisaFuncionarios(funcionarios)){
+                    System.out.println("Funcionario ja cadastrado");
+                    cadastro.nextLine();
+                    return;
+                }else{
+                    //codigo com auto incremento
+                    funcionarios.setCodigo(PersistenciaFuncionarios.listaFuncionarios.size()+1);
+                    do{
+                        System.out.println("Digite o nome"); 
+                        dado = cadastro.nextLine();
+                        if(dado.isEmpty()){
+                            System.out.println("Nome é um campo obrigatório");
                         }else{
-                             System.out.println("Erro:CEP invalido");
-                             System.out.println("Tente novamente");
+                            funcionarios.setNome(dado);
                         }
+                    }while(dado.isEmpty());
+                    
+                    System.out.println("Digite o telefone");
+                    dado = cadastro.nextLine();
+                    if(dado.isEmpty()){
+                        funcionarios.setTefefone(" - ");
                     }else{
-                        System.out.println("CPF INVÁLIDO!");
-                        System.out.println("Tente novamente");
+                        funcionarios.setTefefone(dado);
                     }
+                    
+                    do{
+                        System.out.println("Digite o cargo");
+                        dado= cadastro.nextLine();
+                        if(dado.isEmpty()){
+                            System.out.println("Cargo é obrigatório");
+                        }else{
+                            funcionarios.setCargo(dado);
+                        }
+                    }while(dado.isEmpty());
+                    System.out.println("Digite o usuário");
+                    dado = cadastro.nextLine();
+                    if(dado.isEmpty()){
+                        funcionarios.setTefefone(" - ");
+                    }else{
+                        funcionarios.setTefefone(dado);
+                    }
+                    System.out.println("Digite a senha");
+                    dado = cadastro.nextLine();
+                    if(dado.isEmpty()){
+                        funcionarios.setTefefone(" - ");
+                    }else{
+                        funcionarios.setTefefone(dado);
+                    }
+                    //cadsatro de endereco do funcionario
+                    endereco = visaoEndereco.cadastrarEndereco();
+                    if(endereco == null){
+                        System.out.println("CEP Invalido");
+                        cadastro.nextLine();
+                        return;
+                    }else{
+                        funcionarios.setEndereco(endereco);
+                    }
+                    funcionarios.setCodigo(PersistenciaFuncionarios.listaFuncionarios.size()+1);
+                    boolean operacao = persistenciaFuncionarios.salvar(funcionarios);
+                    if (operacao) {
+                      System.out.println("Salvo com sucesso");
+                    }
+                    else {
+                       System.out.println("Erro!");
+                    }
+                }
+            }catch(IOException ex){
+                System.out.println("Erro na escrita ou leitura do arquivo");
+            }
+                
             }
             
-        } catch (FileNotFoundException ex) {
-            System.out.println("Erro! Arquivo nao encontrado");
-        } catch (IOException ex) {
-            System.out.println("Erro na leitura ou escrita do arquivo");
         }
+     public void imprimeFuncionarios(Funcionarios funcionario){
+        System.out.println("Codigo: " + funcionario.getCodigo());
+        System.out.println("Nome: " + funcionario.getNome());
+        System.out.println("CPF: " + funcionario.getCpf());
+        System.out.println("Telefone: " + funcionario.getTefefone());
+        System.out.println("Cargo: " + funcionario.getCargo());
         
-    }
+        System.out.println("---------------- Endereco ----------------");
+        System.out.println("Rua: " + funcionario.getEndereco().getRua());
+        System.out.println("Numero: " + funcionario.getEndereco().getNumero());
+        System.out.println("Complemento: " + funcionario.getEndereco().getComplemento());
+        System.out.println("Bairro: " + funcionario.getEndereco().getBairro());
+        System.out.println("Cidade: " + funcionario.getEndereco().getCidade());
+        System.out.println("CEP: " + funcionario.getEndereco().getCep());
+        System.out.println("UF: " + funcionario.getEndereco().getUf());
+     }   
 }
 
