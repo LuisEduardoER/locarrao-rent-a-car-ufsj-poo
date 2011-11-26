6bfbf5ade155a94cb74edad3f7bbcc29230eb22a
joao.dk16 <joao.dk16@gmail.com>
2011-11-26 14:45:14 +0000

    alteração em login. Passando os dados do funcionario para a tela do menu

--- Trabalho_de_POO/src/locarrao/visao/grafica/VisaoLogin.java
+++ Trabalho_de_POO/src/locarrao/visao/grafica/VisaoLogin.java
@@ -124,6 +124,7 @@ public class VisaoLogin extends javax.swing.JFrame {
         else{
             log.info(formatador.format(new Date()) + " " + funcionario.getCpf() + 
                     " logou-se como "+ funcionario.getCargo() );
+            VisaoMenu.funcionario = funcionario;
         }
         
         
