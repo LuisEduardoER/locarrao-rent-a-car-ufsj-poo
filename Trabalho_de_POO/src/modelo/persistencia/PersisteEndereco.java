e68ebbe319093ab8f2a1b6a8d9506226d6705835
joao.dk16 <joao.dk16@gmail.com>
2011-11-26 18:41:42 +0000

    validação do dos dados de cliente

--- Trabalho_de_POO/src/modelo/persistencia/PersisteEndereco.java
+++ Trabalho_de_POO/src/modelo/persistencia/PersisteEndereco.java
@@ -3,6 +3,9 @@ package modelo.persistencia;
 import modelo.dominio.Endereco;
 
 public class PersisteEndereco extends DaoBase {
+    /** Salva o endereço no banco de dados.
+     * 
+     */
     public void salvarBD(Endereco endereco){
         abrirDB();
         em.persist(endereco);
