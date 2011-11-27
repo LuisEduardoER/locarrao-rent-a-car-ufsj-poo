e5dc22b15934da06960af641f8045b0e33f9abed
thaisccoufsj <thaisccoufsj@52dbeed6-62d6-36f8-2340-52c633b637ce>
2011-11-27 23:15:33 +0000

    corrigindo erros.

--- trunk/Trabalho_de_POO/test/modelo/persistencia/PersisteMarcaVeiculoTest.java
+++ trunk/Trabalho_de_POO/test/modelo/persistencia/PersisteMarcaVeiculoTest.java
@@ -90,7 +90,7 @@ public class PersisteMarcaVeiculoTest {
         System.out.println("retornarTodosModelosBD");
         PersisteMarcaVeiculo instance = new PersisteMarcaVeiculo();
         List expResult = null;
-        List result = instance.retornarTodosModelosBD();
+        List result = instance.retornarTodasMarcasBD();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
